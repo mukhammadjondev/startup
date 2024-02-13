7627a3c43cb1d0e99f3e7a801109a7935bd39aef
mukhammadjondev <muhsdev@gmail.com>
2024-02-13 12:01:28 +0500

    Payment method & Recieve email

--- src/components/checkout-form/chekout-form.tsx
+++ src/components/checkout-form/chekout-form.tsx
@@ -1,76 +1,191 @@
-import { getTotalPrice } from '@/helpers/total-price.helper';
-import { useTypedSelector } from '@/hooks/useTypedSelector';
-import { Button } from '@chakra-ui/react';
-import { AddressElement, PaymentElement, useElements, useStripe } from '@stripe/react-stripe-js';
-import { FormEvent, useEffect, useState } from 'react';
-import ErrorAlert from '../error-alert/error-alert';
+import $axios from "@/api/axios";
+import { getMailUrl } from "@/config/api.config";
+import { getTotalPrice } from "@/helpers/total-price.helper";
+import { useActions } from "@/hooks/useActions";
+import { useTypedSelector } from "@/hooks/useTypedSelector";
+import {
+  Box,
+  Button,
+  Flex,
+  Stack,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import {
+  AddressElement,
+  CardCvcElement,
+  CardExpiryElement,
+  CardNumberElement,
+  useElements,
+  useStripe,
+} from "@stripe/react-stripe-js";
+import { StripeAddressElement } from "@stripe/stripe-js";
+import { useRouter } from "next/router";
+import { useState } from "react";
+import ErrorAlert from "../error-alert/error-alert";
 
 export default function CheckoutForm() {
-	const stripe = useStripe()
-	const elements = useElements()
-	const { courses, books} = useTypedSelector(state => state.cart)
+  const stripe = useStripe();
+  const elements = useElements();
+  const { courses, books } = useTypedSelector((state) => state.cart);
 
-	const [message, setMessage] = useState<string | null>(null)
-	const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string>("");
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const { colorMode } = useColorMode();
+  const router = useRouter();
+  const { getBooks } = useActions();
 
-	useEffect(() => {
-		if (!stripe) return
+  const cardStyles = {
+    base: {
+      color: colorMode === "light" ? "#000" : "#fff",
+      fontSmoothing: "antialiased",
+      fontSize: "16px",
+      "::placeholder": {
+        color:
+          colorMode === "light" ? "rgba(0,0,0,.5)" : "rgba(255,255,255,.4)",
+        opacity: "0.7",
+      },
+    },
+    ivalid: {
+      color: "#fa755a",
+      iconColor: "#fa755a",
+    },
+  };
 
-		const clientSecret = new URLSearchParams(window.location.search).get(
-			'payment_intent_client_secret'
-		)
+  const handleSubmit = async () => {
+    if (!stripe || !elements) return;
 
-		if (!clientSecret) return
+    setIsLoading(true);
 
-		stripe.retrievePaymentIntent(clientSecret).then(({ paymentIntent }) => {
-			switch (paymentIntent?.status) {
-				case 'succeeded':
-					setMessage('Payment succeeded!')
-					break
-				case 'processing':
-					setMessage('Your payment is processing.')
-					break
-				case 'requires_payment_method':
-					setMessage('Your payment was not successful, please try again.')
-					break
-				default:
-					setMessage('Something went wrong.')
-					break
-			}
-		})
-	}, [stripe])
+    const addressElement = elements.getElement(
+      "address"
+    ) as StripeAddressElement;
 
-	const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-		e.preventDefault()
+    const { value } = await addressElement.getValue();
 
-		if (!stripe || !elements) return
+    const { error, paymentMethod } = await stripe.createPaymentMethod({
+      // @ts-ignore
+      type: "card",
+      card: elements.getElement(CardNumberElement),
+      billing_details: {
+        address: value.address,
+        name: `${value.firstName} ${value.lastName}`,
+      },
+    });
 
-		setIsLoading(true)
+    if (error) {
+      setError(`Your payment deatails couldn't be verified: ${error.message}`);
+      setIsLoading(false);
+    } else {
+      const { data } = await $axios.post(`/payment/books`, {
+        price: getTotalPrice(courses, books),
+        paymentMethod: paymentMethod.id,
+      });
 
-		const { error } = await stripe.confirmPayment({
-			elements,
-			confirmParams: {
-				return_url: `${process.env.NEXT_PUBLIC_CLIENT_DOMAIN!}/shop/success`,
-			},
-		})
-		if (error.type === 'card_error' || error.type === 'validation_error') {
-			setMessage(error.message as string)
-		} else {
-			setMessage('An unexpected error occurred.')
-		}
+      const payload = await stripe.confirmCardPayment(data);
 
-		setIsLoading(false)
-	}
+      if (payload.error) {
+        setIsLoading(false);
+        setError(
+          `Your payment deatails couldn't be verified: ${payload.error.message}`
+        );
+      } else {
+        for (const book of books) {
+          await $axios.post(`${getMailUrl("books")}/${book._id}`);
+        }
+        getBooks([]);
+        router.push("/shop/success");
+      }
+    }
+  };
 
-	return (
-		<form id='payment-form' onSubmit={handleSubmit}>
-			{message && <ErrorAlert title={message} clearHandler={() => setMessage(null)} />}
+  return (
+    <Stack>
+      {error && <ErrorAlert title={error} clearHandler={() => setError("")} />}
 
-			<PaymentElement id='payment-element' options={{layout: 'tabs'}} />
-			<AddressElement options={{mode: 'billing'}} />
-			<Button w='full' h={14} mt={5} isDisabled={isLoading || !stripe || !elements} isLoading={isLoading} boxShadow='xl' type='submit'>
-				Pay now {getTotalPrice(courses, books).toLocaleString('en-US', {style: 'currency', currency: 'USD'})}
-			</Button>
-		</form>
-	)
+      <Flex gap={2}>
+        <Box
+          px={2}
+          py={3}
+          w="60%"
+          boxShadow={
+            colorMode === "dark"
+              ? "0px 2px 4px rgba(0, 0, 0, 0.5), 0px 1px 6px rgba(0, 0, 0, 0.25)"
+              : ""
+          }
+          borderRadius="md"
+          border="1px"
+          borderColor={useColorModeValue(
+            "rgba(0,0,0,.1)",
+            "rgba(255,255,255,.1)"
+          )}
+          bg={useColorModeValue("white", "#30303d")}
+        >
+          <CardNumberElement
+            options={{
+              style: cardStyles,
+              placeholder: "XXXX XXXX XXXX XXXX",
+              showIcon: true,
+            }}
+          />
+        </Box>
+        <Box
+          px={2}
+          w="20%"
+          py={3}
+          boxShadow={
+            colorMode === "dark"
+              ? "0px 2px 4px rgba(0, 0, 0, 0.5), 0px 1px 6px rgba(0, 0, 0, 0.25)"
+              : ""
+          }
+          borderRadius="md"
+          border="1px"
+          borderColor={useColorModeValue(
+            "rgba(0,0,0,.1)",
+            "rgba(255,255,255,.1)"
+          )}
+          bg={useColorModeValue("white", "#30303d")}
+        >
+          <CardExpiryElement options={{ style: cardStyles }} />
+        </Box>
+        <Box
+          px={2}
+          w="20%"
+          py={3}
+          boxShadow={
+            colorMode === "dark"
+              ? "0px 2px 4px rgba(0, 0, 0, 0.5), 0px 1px 6px rgba(0, 0, 0, 0.25)"
+              : ""
+          }
+          borderRadius="md"
+          border="1px"
+          borderColor={useColorModeValue(
+            "rgba(0,0,0,.1)",
+            "rgba(255,255,255,.1)"
+          )}
+          bg={useColorModeValue("white", "#30303d")}
+        >
+          <CardCvcElement
+            options={{ style: cardStyles, placeholder: "Security code" }}
+          />
+        </Box>
+      </Flex>
+      <AddressElement options={{ mode: "billing" }} />
+      <Button
+        w="full"
+        h={14}
+        mt={5}
+        isDisabled={isLoading || !stripe || !elements}
+        isLoading={isLoading}
+        boxShadow="xl"
+        onClick={handleSubmit}
+      >
+        Pay now{" "}
+        {getTotalPrice(courses, books).toLocaleString("en-US", {
+          style: "currency",
+          currency: "USD",
+        })}
+      </Button>
+    </Stack>
+  );
 }
--- src/page-component/shop-page-component/checkout-page-component.tsx
+++ src/page-component/shop-page-component/checkout-page-component.tsx
@@ -1,79 +1,85 @@
-import { CheckoutForm } from "@/components"
-import SectionTitle from "@/components/section-title/section-title"
-import { loadImage } from "@/helpers/image.helper"
-import { getTotalPrice } from "@/helpers/total-price.helper"
-import { useTypedSelector } from "@/hooks/useTypedSelector"
-import { PaymentService } from "@/services/payment.service"
-import { Box, Divider, Grid, GridItem, HStack, Skeleton, Stack, Text, useColorModeValue } from "@chakra-ui/react"
-import { Elements } from "@stripe/react-stripe-js"
-import { loadStripe } from "@stripe/stripe-js"
-import Image from "next/image"
-import { Fragment, useEffect, useState } from "react"
+import { CheckoutForm } from "@/components";
+import SectionTitle from "@/components/section-title/section-title";
+import { loadImage } from "@/helpers/image.helper";
+import { useTypedSelector } from "@/hooks/useTypedSelector";
+import {
+  Box,
+  Divider,
+  Grid,
+  GridItem,
+  HStack,
+  Text,
+  useColorMode,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+import Image from "next/image";
+import { Fragment } from "react";
 
-const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string)
+const stripePromise = loadStripe(
+  process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY as string
+);
 
 const CheckoutPageComponent = () => {
-  const [clientSecret, setClientSecret] = useState('')
+  const { books } = useTypedSelector((state) => state.cart);
+  const { colorMode } = useColorMode();
 
-  const { books, courses } = useTypedSelector(state => state.cart)
-
-  useEffect(() => {
-    const getClientSecret = async () => {
-      const response = await PaymentService.paymentBooks(getTotalPrice(courses, books))
-      setClientSecret(response)
-    }
-
-    getClientSecret()
-  }, [])
-
-  return <>
-    <SectionTitle
-      title='Checkout'
-      subtitle='We’re on a mission to deliver engaging, curated courses at a reasonable price.'
-    />
-    <Grid gridTemplateColumns='70% 30%' gap={5}>
-      <GridItem>
-        <Divider my={5} />
-        {clientSecret ? (
-          <Elements stripe={stripePromise} options={{clientSecret, appearance: {theme: 'night'}}}>
+  return (
+    <>
+      <SectionTitle
+        title="Checkout"
+        subtitle="We’re on a mission to deliver engaging, curated courses at a reasonable price."
+      />
+      <Grid gridTemplateColumns="70% 30%" gap={5}>
+        <GridItem>
+          <Divider my={5} />
+          <Elements
+            stripe={stripePromise}
+            options={{
+              appearance: { theme: colorMode === "dark" ? "night" : "stripe" },
+            }}
+          >
             <CheckoutForm />
           </Elements>
-        ) : (
-          <Stack>
-            <HStack mt={10}>
-              <Skeleton height={12} w='50%' />
-              <Skeleton height={12} w='25%' />
-              <Skeleton height={12} w='25%' />
-            </HStack>
-            <Skeleton height={12} w='100%' mt={5} />
-            <Skeleton height={12} w='100%' mt={5} />
-            <Skeleton height={14} w='100%' mt={5} />
-          </Stack>
-        )}
-      </GridItem>
-      <GridItem mt={10} borderLeft='1px' p={5} borderColor={useColorModeValue('gray.200', 'gray.700')}>
-        <Text fontSize='2xl' fontWeight='bold'>
-          Order details
-        </Text>
-        {books.map(book => (
-          <Fragment key={book._id}>
-            <HStack justify='space-between'>
-              <HStack>
-                <Box pos='relative' w='40px' h='30px'>
-                  <Image src={loadImage(book.image)} fill alt={book.title} style={{objectFit: 'cover'}} />
-                </Box>
-                <Text>{book.title}</Text>
+        </GridItem>
+        <GridItem
+          mt={10}
+          borderLeft="1px"
+          p={5}
+          borderColor={useColorModeValue("gray.200", "gray.700")}
+        >
+          <Text fontSize="2xl" fontWeight="bold">
+            Order details
+          </Text>
+          {books.map((book) => (
+            <Fragment key={book._id}>
+              <HStack justify="space-between">
+                <HStack>
+                  <Box pos="relative" w="40px" h="30px">
+                    <Image
+                      src={loadImage(book.image)}
+                      fill
+                      alt={book.title}
+                      style={{ objectFit: "cover" }}
+                    />
+                  </Box>
+                  <Text>{book.title}</Text>
+                </HStack>
+                <Text fontWeight="bold" color="facebook.500">
+                  {book.price.toLocaleString("en-US", {
+                    style: "currency",
+                    currency: "USD",
+                  })}
+                </Text>
               </HStack>
-              <Text fontWeight='bold' color='facebook.500'>
-                {book.price.toLocaleString('en-US', { style: 'currency', currency: 'USD' })}
-              </Text>
-            </HStack>
-            <Divider my={5} />
-          </Fragment>
-        ))}
-      </GridItem>
-    </Grid>
-  </>
-}
+              <Divider my={5} />
+            </Fragment>
+          ))}
+        </GridItem>
+      </Grid>
+    </>
+  );
+};
 
-export default CheckoutPageComponent
+export default CheckoutPageComponent;
